edaac7e0fd1d7c58440a6e28e222c6248d7600f4
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-10 19:45:14 +0300

    refactoring

--- src/main/java/training/katas/exes_and_ohs/XO.java
+++ src/main/java/training/katas/exes_and_ohs/XO.java
@@ -8,9 +8,6 @@ public class XO {
     var lowerCasedString = str.toLowerCase();
     var x = 'x';
     var o = 'o';
-    System.out.println(lowerCasedString);
-    System.out.println(
-        lowerCasedString.chars().filter(i -> i == x).count() == lowerCasedString.chars().filter(i -> i == o).count());
     return lowerCasedString.chars().filter(i -> i == x).count() == lowerCasedString.chars().filter(i -> i == o).count();
   }
 }
