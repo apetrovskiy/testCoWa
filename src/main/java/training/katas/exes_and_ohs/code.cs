9c5d36f5a82a072ab79f729a1385c93bed00dc64
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-10 20:00:33 +0300

    C# finished

---
+++ src/main/java/training/katas/exes_and_ohs/code.cs
@@ -0,0 +1,21 @@
+namespace Solution
+{
+    using System.Linq;
+    using System;
+
+    public static class Kata
+    {
+        public static bool XO(string input)
+        {
+            // return //Code it!
+            var lowerCasedString = input.ToLower();
+            var x = 'x';
+            var o = 'o';
+            return lowerCasedString
+            .Where(i => i == x)
+            .Count() == lowerCasedString
+            .Where(i => i == o)
+            .Count();
+        }
+    }
+}
