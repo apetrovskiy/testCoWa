1e0457fa4158b37cee10ff0eff075a6c2534d174
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-10 19:43:57 +0300

    finished Java

--- src/main/java/training/katas/exes_and_ohs/XO.java
+++ src/main/java/training/katas/exes_and_ohs/XO.java
@@ -1,13 +1,16 @@
 package training.katas.exes_and_ohs;
 
 public class XO {
-  
-    public static boolean getXO (String str) {
-      
-      // Good Luck!!
 
-      return false;
-      
-    }
+  public static boolean getXO(String str) {
+
+    // Good Luck!!
+    var lowerCasedString = str.toLowerCase();
+    var x = 'x';
+    var o = 'o';
+    System.out.println(lowerCasedString);
+    System.out.println(
+        lowerCasedString.chars().filter(i -> i == x).count() == lowerCasedString.chars().filter(i -> i == o).count());
+    return lowerCasedString.chars().filter(i -> i == x).count() == lowerCasedString.chars().filter(i -> i == o).count();
   }
-  
+}
