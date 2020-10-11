d5a57f177c8aac3e5d8949ac29801f1ec645e472
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-12 02:28:06 +0300

    .

---
+++ src/main/java/training/katas/number_of_people_in_the_bus/Metro.java
@@ -0,0 +1,10 @@
+package main.java.training.katas.number_of_people_in_the_bus;
+
+import java.util.ArrayList;
+
+class Metro {
+
+  public static int countPassengers(ArrayList<int[]> stops) {
+  //Code here!
+  }
+}
--- src/main/java/training/katas/number_of_people_in_the_bus/code.py
+++ src/main/java/training/katas/number_of_people_in_the_bus/code.py
@@ -1,3 +1,3 @@
 def number(bus_stops):
     # Good Luck!
-    return 1
+    return sum([x[0] for x in bus_stops]) - sum([x[1] for x in bus_stops])
