e24231e181347dca4d99faf24609079a29453cb0
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-15 05:26:28 +0300

    Split Strings Python is done

---
+++ src/main/java/training/katas/SplitStrings/split_strings.py
@@ -0,0 +1,6 @@
+def solution(s):
+    print(s)
+    if len(s) == 0:
+        return []
+    print([s[index] + (s[index + 1] if index + 1 < len(s) else '_') for index in range(0, len(s), 2)])
+    return [s[index] + (s[index + 1] if index + 1 < len(s) else '_') for index in range(0, len(s), 2)]
