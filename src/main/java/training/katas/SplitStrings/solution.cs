21904d14f737472acb466657df9ab268dc29b986
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-24 05:49:15 +0300

    Split Strings C# is done

--- src/main/java/training/katas/SplitStrings/solution.cs
+++ src/main/java/training/katas/SplitStrings/solution.cs
@@ -1,10 +1,19 @@
 namespace training.katas.SplitStrings
 {
+    using System;
+    using System.Linq;
+
     public class SplitString
     {
         public static string[] Solution(string str)
         {
-            return null;
+            if (string.IsNullOrEmpty(str))
+                return Array.Empty<string>();
+            return Enumerable.Range(0, str.Length)
+                .Where(index => index % 2 == 0)
+                .Select(index => str.Substring(index, 1) + (index + 1 < str.Length ? str.Substring(index + 1, 1) : "_"))
+                .ToList()
+                .ToArray();
         }
     }
 }
