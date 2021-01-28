8f217fa75bca0227801ff3ff4b659b9dd67cb603
Alexander <apetrovskiy@hotmail.com>
2021-01-28 08:10:57 +0300

    Python is done

---
+++ src/test/java/training/katas/Nesting Structure ComparisonNestingStructureComparison/test_nesting_structure_comparison.py
@@ -0,0 +1,41 @@
+from src.main.java.training.katas.NestingStructureComparison.nesting_structure_comparison import same_structure_as
+from typing import List
+import pytest
+
+
+test_data = [
+    ([1, [1, 1]], [2, [2, 2]], True),
+    ([1, [1, 1]], [[2, 2], 2], False),
+    ([], 1, False),
+    ([1, '[', ']'], ['[', ']', 1], True)
+]
+
+
+@ pytest.mark.parametrize("original,other,expected_result", test_data)
+def test_nesting_structure_comparison(original: List[int], other: List[int], expected_result: bool):
+    assert expected_result == same_structure_as(original, other)
+
+
+'''
+# should return True
+same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+
+# should return False
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
+
+# should return True
+same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
+
+# should return False
+same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
+'''
+
+'''
+Test.assert_equals(same_structure_as(
+    [1,[1,1]],[2,[2,2]]), True, "[1,[1,1]] same as [2,[2,2]]")
+Test.assert_equals(same_structure_as([1,[1,1]],[[2,2],2]), False, "[1,[1,1]] not same as [[2,2],2]")Test.assert_equals(same_structure_as([1,[1,1]],[2,[2,2]]), True, "[1,[1,1]] same as [2,[2,2]]")
+Test.assert_equals(same_structure_as(
+    [1,[1,1]],[[2,2],2]), False, "[1,[1,1]] not same as [[2,2],2]")
+'''
--- src/test/java/training/katas/WhereMyAnagramsAt/test_where_my_anagrams_at.py
+++ src/test/java/training/katas/WhereMyAnagramsAt/test_where_my_anagrams_at.py
@@ -1,3 +1,19 @@
+from src.main.java.training.katas.WhereMyAnagramsAt.where_my_anagrams_at import anagrams
+from typing import List
+import pytest
+
+
+test_data = [
+    ('abba', ['aabb', 'abcd', 'bbaa', 'dada'], ['aabb', 'bbaa']),
+    ('racer', ['crazer', 'carer', 'racar',
+               'caers', 'racer'], ['carer', 'racer']),
+    ('laser', ['lazing', 'lazy',  'lacer'], [])
+]
+
+
+@ pytest.mark.parametrize("word,words,expected_result", test_data)
+def test_where_my_anagrams_at(word: str, words: List[str], expected_result: List[str]):
+    assert expected_result == anagrams(word, words)
 
 
 '''
