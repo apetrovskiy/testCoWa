c09ad2e2db0f6159ccf6ed31b343d548182ec585
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-29 00:12:54 +0300

    Create tribonacci_sequence_test.dart

---
+++ src/test/java/training/katas/TribonacciSequence/tribonacci_sequence_test.dart
@@ -0,0 +1,9 @@
+library training.katas.TribonacciSequence;
+
+import "package:test/test.dart";
+
+test("Test Description", () {  
+   var actualValue = 1;
+   var matchingValue = 1;
+   expect(actualValue , matchingValue) 
+});
