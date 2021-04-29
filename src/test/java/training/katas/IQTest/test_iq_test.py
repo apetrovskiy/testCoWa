e040ae77556ee06d93f6c604c47a3397f43f9808
Alexander <apetrovskiy@hotmail.com>
2021-04-29 08:16:33 +0300

    Update test_iq_test.py

--- src/test/java/training/katas/IQTest/test_iq_test.py
+++ src/test/java/training/katas/IQTest/test_iq_test.py
@@ -7,9 +7,10 @@ from src.main.java.training.katas.IQTest.iq_test import iq_test
 
 test_data = [
     ([2, 4, 7, 8, 10], 3),
-    ([1, 2, 2],1)
+    ([1, 2, 2], 1)
 ]
 
-@ pytest.mark.parametrize("input_array,expected_result",test_data)
-def test_iq_test(input_array:List[int], expected_result:int):
-    assert expected_result==iq_test(input_array)
+
+@ pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_iq_test(input_array: List[int], expected_result: int):
+    assert expected_result == iq_test(input_array)
