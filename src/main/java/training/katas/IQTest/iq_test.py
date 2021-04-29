e24cb74a195623100ee6b98c7e40427565f44d31
Alexander <apetrovskiy@hotmail.com>
2021-04-29 11:00:45 +0300

    Update iq_test.py

--- src/main/java/training/katas/IQTest/iq_test.py
+++ src/main/java/training/katas/IQTest/iq_test.py
@@ -1,3 +1,13 @@
 def iq_test(numbers):
-    #your code here
+    # your code here
+    selection = [x % 2 for x in range(0, 3)]
+    search_for_even: bool = False if selection.count(
+        0) > selection.count(1) else True
+    for i in range(len(numbers)):
+        if search_for_even:
+            if numbers[i] % 2 == 0:
+                return i+1
+        else:
+            if numbers[i] % 2 == 1:
+                return i+1
     return 0
