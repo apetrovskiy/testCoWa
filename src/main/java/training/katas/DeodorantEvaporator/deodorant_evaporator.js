a7f005e67a54620bd001051d1352084403f8a405
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-30 18:18:05 +0300

    Update deodorant_evaporator.js

--- src/main/java/training/katas/DeodorantEvaporator/deodorant_evaporator.js
+++ src/main/java/training/katas/DeodorantEvaporator/deodorant_evaporator.js
@@ -1,3 +1,9 @@
-function evaporator(content, evap_per_day, threshold){ 
-  return -1;
+function evaporator(content, evap_per_day, threshold) {
+    var nthDay = 0;
+    var absoluteThreshold = (100 - threshold) / 100;
+    while (content > absoluteThreshold) {
+        nthDay += 1;
+        content *= (100 - evap_per_day) / 100;
+    }
+    return nthDay;
 }
