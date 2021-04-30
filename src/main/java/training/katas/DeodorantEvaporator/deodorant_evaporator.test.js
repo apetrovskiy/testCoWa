d65329a56c2329e35be907bcacb9355d1f1767bb
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-30 17:41:00 +0300

    Update deodorant_evaporator.test.js

--- src/main/java/training/katas/DeodorantEvaporator/deodorant_evaporator.test.js
+++ src/main/java/training/katas/DeodorantEvaporator/deodorant_evaporator.test.js
@@ -3,15 +3,14 @@
 // import { JasmineAllureReporter } from "allure-jasmine";
 // import { AllureRuntime, Status, TestResult } from "allure-js-commons";
 const chai = require("chai");
-const pickPeaks = require('../deodorant_evaporator');
+const evaporator = require("../deodorant_evaporator");
 const assert = chai.assert;
 chai.config.truncateThreshold = 0;
 
 describe("Sample tests", function() {
-it("Tests", function() {
-    assert.equal(evaporator(10, 10, 10), 2);
-    assert.equal(evaporator(10, 10, 5), 29);
-    assert.equal(evaporator(100, 5, 5), 59);
-});
-});
+    it("Tests", function() {
+        assert.equal(evaporator(10, 10, 10), 2);
+        assert.equal(evaporator(10, 10, 5), 29);
+        assert.equal(evaporator(100, 5, 5), 59);
+    });
 });
