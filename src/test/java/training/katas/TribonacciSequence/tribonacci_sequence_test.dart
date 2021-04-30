b007f3f8cda0eaa90645cab4f19d04a2748dd488
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-30 11:57:24 +0300

    Update tribonacci_sequence_test.dart

--- src/test/java/training/katas/TribonacciSequence/tribonacci_sequence_test.dart
+++ src/test/java/training/katas/TribonacciSequence/tribonacci_sequence_test.dart
@@ -10,12 +10,12 @@ test("Test Description", () {
 });
 */
 
-/*
 import "package:test/test.dart";
-import "package:solution/solution.dart";
+// import "package:solution/solution.dart";
 import "../../../../../main/java/training/katas/TribonacciSequence/tribonacci_sequence.dart";
 
-void tester(List<num> sig, int n, List<num> exp) => test("tribonacci($sig, $n)", () => expect(tribonacci(sig, n), equals(exp)));
+void tester(List<num> sig, int n, List<num> exp) =>
+    test("tribonacci($sig, $n)", () => expect(tribonacci(sig, n), equals(exp)));
 void main() {
   group("Basic tests", () {
     tester([1, 1, 1], 10, [1, 1, 1, 3, 5, 9, 17, 31, 57, 105]);
@@ -27,7 +27,40 @@ void main() {
     tester([3, 2, 1], 10, [3, 2, 1, 6, 9, 16, 31, 56, 103, 190]);
     tester([1, 1, 1], 1, [1]);
     tester([300, 200, 100], 0, []);
-    tester([0.5, 0.5, 0.5], 30, [0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5]);
-  }); 
+    tester(
+        [0.5, 0.5, 0.5],
+        30,
+        [
+          0.5,
+          0.5,
+          0.5,
+          1.5,
+          2.5,
+          4.5,
+          8.5,
+          15.5,
+          28.5,
+          52.5,
+          96.5,
+          177.5,
+          326.5,
+          600.5,
+          1104.5,
+          2031.5,
+          3736.5,
+          6872.5,
+          12640.5,
+          23249.5,
+          42762.5,
+          78652.5,
+          144664.5,
+          266079.5,
+          489396.5,
+          900140.5,
+          1655616.5,
+          3045153.5,
+          5600910.5,
+          10301680.5
+        ]);
+  });
 }
-*/
