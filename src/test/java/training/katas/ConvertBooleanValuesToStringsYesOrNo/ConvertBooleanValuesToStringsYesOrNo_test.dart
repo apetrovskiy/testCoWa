2c02dbb8e3eac1852ba8992ea20a935ea5e0baa3
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-30 11:44:55 +0300

    dart fixes

--- src/test/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo_test.dart
+++ src/test/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo_test.dart
@@ -4,11 +4,12 @@ import "package:test/test.dart";
 // import "package:../../../../../main/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo.dart";
 // import "package:..\\..\\..\\..\\..\\main\\java\\training\\katas\\ConvertBooleanValuesToStringsYesOrNo\\ConvertBooleanValuesToStringsYesOrNo.dart";
 // import "..\\..\\..\\..\\..\\main\\java\\training\\katas\\ConvertBooleanValuesToStringsYesOrNo\\ConvertBooleanValuesToStringsYesOrNo.dart";
+import '../../../../../main/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo.dart';
 
 void main() {
   test('Tests', () {
-    // expect(bool_to_word(true), equals("Yes"));
-    // expect(bool_to_word(false), equals("No"));
+    expect(bool_to_word(true), equals("Yes"));
+    expect(bool_to_word(false), equals("No"));
     expect(1, equals(1));
   });
 }
