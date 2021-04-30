477a2782ae83366eb5dcb68abf0bdfe86d1f844a
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-30 16:38:41 +0300

    refactoring

--- src/test/java/training/katas/DeodorantEvaporator/deodorant_evaporator_test.dart
+++ src/test/java/training/katas/DeodorantEvaporator/deodorant_evaporator_test.dart
@@ -3,7 +3,7 @@ library training.katas.DeodorantEvaporator;
 // See https://pub.dartlang.org/packages/test
 import "package:test/test.dart";
 // import "package:solution/solution.dart";
-import '../../../../../main/java/training/katas/DeodorantEvaporator/solution.dart';
+import '../../../../../main/java/training/katas/DeodorantEvaporator/deodorant_evaporator_test.dart';
 
 void main() {
   test("Sample Deodorant Evaporator Test Cases", () {
