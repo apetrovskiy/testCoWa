3e6f254b481f7b987cef7e134a5b72fa82147acc
Alexander <apetrovskiy@hotmail.com>
2021-05-01 21:28:54 +0300

    preparation + Python

--- src/test/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo_test.dart
+++ src/test/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo_test.dart
@@ -5,7 +5,7 @@ import "package:test/test.dart";
 import '../../../../../main/java/training/katas/ConvertBooleanValuesToStringsYesOrNo/ConvertBooleanValuesToStringsYesOrNo.dart';
 
 void main() {
-  group('Basic tests', () {
+  group('Convert boolen value basic tests', () {
     test('Yes/No', () {
       expect(bool_to_word(true), equals("Yes"));
       expect(bool_to_word(false), equals("No"));
