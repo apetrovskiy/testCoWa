04a303b96148cdc71a052f29c47bfb9dbcc71368
Alexander <apetrovskiy@hotmail.com>
2021-05-02 17:03:28 +0300

    go fixes

--- src/main/java/training/katas/BitCounting/BitCounting.go
+++ src/main/java/training/katas/BitCounting/BitCounting.go
@@ -1,5 +1,5 @@
 package BitCounting
 
 func CountBits(uint) int {
-return 0
+	return 0
 }
--- src/main/java/training/katas/BitCounting/bit_counting_test.go
+++ src/main/java/training/katas/BitCounting/bit_counting_test.go
@@ -1,20 +1,21 @@
 package BitCounting_test
 
 import (
-  . "github.com/onsi/ginkgo"
-  . "github.com/onsi/gomega"
-  . "codewarrior/kata"
-  "github.com/dailymotion/allure-go"
-  "testing"
+	. "codewarrior/kata"
+	"github.com/dailymotion/allure-go"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"testing"
 )
+
 var _ = Describe("CountBits()", func() {
-  It("basic tests", func() {
-    Expect(CountBits(0)).To(Equal(0))
-    Expect(CountBits(4)).To(Equal(1))
-    Expect(CountBits(7)         ).To(Equal(3))
-    Expect(CountBits(9)         ).To(Equal(2))
-    Expect(CountBits(10)        ).To(Equal(2))
-  })
+	It("basic tests", func() {
+		Expect(CountBits(0)).To(Equal(0))
+		Expect(CountBits(4)).To(Equal(1))
+		Expect(CountBits(7)).To(Equal(3))
+		Expect(CountBits(9)).To(Equal(2))
+		Expect(CountBits(10)).To(Equal(2))
+	})
 })
 
 // https://towardsdatascience.com/use-environment-variable-in-your-next-golang-project-39e17c3aaa66
@@ -32,16 +33,18 @@ func goDotEnvVariable(key string) string {
 	return os.Getenv(key)
 }
 
-func dotest(content float64, evapPerDay int, threshold int, exp int) {
-	var ans = CountBits(content, evapPerDay, threshold)
+func dotest(input uint, exp int) {
+	var ans = CountBits(input)
 	Expect(ans).To(Equal(exp))
 }
 
-func TestStepDeodorantEvaporator(t *testing.T) {
+func TestStepBitCounting(t *testing.T) {
 	dotenv := goDotEnvVariable("ALLURE_RESULTS_PATH")
 	allure.Test(t, allure.Action(func() {
-		Expect(CountBits(10, 10, 10)).To(Equal(22))
-		Expect(CountBits(10, 10, 5)).To(Equal(29))
-		Expect(CountBits(100, 5, 5)).To(Equal(59))
+		Expect(CountBits(0)).To(Equal(0))
+		Expect(CountBits(4)).To(Equal(1))
+		Expect(CountBits(7)).To(Equal(3))
+		Expect(CountBits(9)).To(Equal(2))
+		Expect(CountBits(10)).To(Equal(2))
 	}))
 }
