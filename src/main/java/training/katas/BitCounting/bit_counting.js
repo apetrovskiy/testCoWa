b40f0d6a9ed4ae07955b617ac6f41521829e36e1
Alexander <apetrovskiy@hotmail.com>
2021-05-02 13:59:06 +0300

    Update bit_counting.js

--- src/main/java/training/katas/BitCounting/bit_counting.js
+++ src/main/java/training/katas/BitCounting/bit_counting.js
@@ -1,8 +1,8 @@
 var countBits = function(n) {
     let counter = 0;
-    const maxExtent = Math.pow(2, 20);
+    const maxExtent = Math.pow(2, 40);
     for (let i = maxExtent; i > 0; i /= 2) {
-        if (n > i) {
+        if (n >= i && n >= 1) {
             n -= i;
             counter++;
         }
