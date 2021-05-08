8a3775fddbeb88774c029fb37fb18f837ee7ea8c
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-08 10:32:18 +0300

    feature/algorithms-001

---
+++ src/main/java/training/katas/ColouredTriangles/coloured_triangles.py
@@ -0,0 +1,21 @@
+R = 'R'
+G = 'G'
+B = 'B'
+
+
+def get_color(color1: str, color2: str) -> str:
+    if color1 == color2:
+        return color1
+    else:
+        return R if color1 != R and color2 != R \
+            else (G if color1 != G and color2 != G else B)
+
+
+def triangle(row):
+    result = row
+    while len(result) > 1 and result != "":
+        current_row = result
+        result = ""
+        for i in range(len(current_row)-1):
+            result += get_color(current_row[i], current_row[i+1])
+    return result
