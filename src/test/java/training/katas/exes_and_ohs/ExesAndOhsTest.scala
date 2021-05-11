ba646e7cda128b63cfd4797087ba97d1701f22cb
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-05-11 13:57:40 +0300

    Scala JUnit 5 tests

--- src/test/java/training/katas/exes_and_ohs/ExesAndOhsTest.scala
+++ src/test/java/training/katas/exes_and_ohs/ExesAndOhsTest.scala
@@ -1,5 +1,9 @@
 package training.katas.exes_and_ohs
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.{Arguments, MethodSource}
+import org.junit.jupiter.params.provider.Arguments.of
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -15,5 +19,22 @@ class ExesAndOhsTest extends AnyFlatSpec with Matchers {
     s"xo($input)" should s"return $expected" in {
       ExesAndOhs.xo(input) should be(expected)
     }
+  }
+}
+
+class ExesAndOhsScalaTest {
+  @ParameterizedTest
+  @MethodSource(Array("getInputData"))
+  def shouldCheckExesAndOhs(input: String, expectedResult: Boolean) = {
+    assertEquals(ExesAndOhs.xo(input), expectedResult)
+  }
+}
+object ExesAndOhsScalaTest {
+  def getInputData(): java.util.stream.Stream[Arguments] = {
+    java.util.stream.Stream.of(
+      of("xo", true),
+      of("xo0", true),
+      of("xxxoo", false)
+    )
   }
 }
